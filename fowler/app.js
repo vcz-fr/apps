c85214be0cdf686b77c361dc4008d0fabb3342ad
Vincenzo Scalzi <vincenzo.scalzi@gekko.fr>
2019-11-04 12:13:30 +0100

    feat(fowler): Block inputs on submit

--- fowler/app.js
+++ fowler/app.js
@@ -1,10 +1,12 @@
 const _id = id => document.getElementById(id);
 const _r = msg => _id("response").value = msg;
 const _notif = msg => _r(`[INFO] An error has occurred: ${msg}`);
+const _block = disable => (_id("action-enc").disabled = disable, _id("action-dec").disabled = disable)
 
 const send = (action, body) => {
     if (!body.key || !body.msg) { 
-        _r("These inputs are required: key, message");
+        _notif("These inputs are required: key, message");
+        _block(false);
         return;
     }
     fetch(`https://api.vcz.fr/fowler/${action}`, {
@@ -20,16 +22,17 @@ const send = (action, body) => {
     })
     .then(_r)
     .catch(_notif)
+    .then(() => _block(false))
 };
 
 const main = action => e => (
+    _block(true),
     e.preventDefault(),
     fd = new FormData(_id("form")),
     send(action, {msg: fd.get("msg"), key: fd.get("key")})
 );
 
 // Actions
-
 _id("action-enc").addEventListener("click", main("enc"));
 _id("action-dec").addEventListener("click", main("dec"));
 
