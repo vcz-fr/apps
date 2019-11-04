95290dc412de4fafa1f301b7c82b96a13a08bbd7
Vincenzo Scalzi <vincenzo.scalzi@gekko.fr>
2019-11-04 10:36:28 +0100

    feat(fowler): Handle empty inputs

--- fowler/app.js
+++ fowler/app.js
@@ -1,20 +1,32 @@
 const _id = id => document.getElementById(id);
-const _v = id => (v = _id(id).value, _id(id).value === ""? null: value);
 const _r = msg => _id("response").value = msg;
 const _notif = msg => _r(`[INFO] An error has occurred: ${msg}`);
 
-const send = (action, body) => fetch(`https://api.vcz.fr/fowler/${action}`, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-        "content-type": "application/json"
+const send = (action, body) => {
+    if (!body.key || !body.msg) { 
+        _r("These inputs are required: key, message");
+        return;
     }
-})
-.then(r => {if (r.ok){ return r.json() } throw "Could not process the inputs"})
-.then(_r)
-.catch(_notif);
+    fetch(`https://api.vcz.fr/fowler/${action}`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then(r => {
+        if (!r.ok){ throw "Could not process the inputs" }
+        return r.json();
+    })
+    .then(_r)
+    .catch(_notif)
+};
 
-const main = action => e => (e.preventDefault(), fd = new FormData(_id("form")), send(action, {msg: fd.get("msg"), key: fd.get("key")}));
+const main = action => e => (
+    e.preventDefault(),
+    fd = new FormData(_id("form")),
+    send(action, {msg: fd.get("msg"), key: fd.get("key")})
+);
 
 // Actions
 
