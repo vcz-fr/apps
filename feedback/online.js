b080ab5faafadba9f2cb2352110be00654980186
Vincenzo Scalzi <vcz-fr@users.noreply.github.com>
2019-11-11 18:07:22 +0100

    feat(feedback): Frontend of the Feedback app

---
+++ feedback/online.js
@@ -0,0 +1,47 @@
+const _q = id => (v = new URLSearchParams(window.location.search).get(id), v === "" ? null : v);
+const _id = id => document.getElementById(id);
+const _r = msg => _id("response").value = msg;
+const _notif = msg => _r(`[INFO] Error: ${msg}`);
+const _block = disable => _id("action-send").disabled = disable
+
+const send = body => {
+    if (!body.appid || !body.msg) {
+        _notif("These inputs are required: appid, message");
+        _block(false);
+        return;
+    }
+    fetch(`https://api.vcz.fr/feedback`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+        .then(r => {
+            if (!r.ok) { throw "Could not process the inputs" }
+            _r("Thanks for your feedback!")
+        })
+        .catch(_notif)
+        .then(() => _block(false))
+};
+
+const main = e => (
+    _block(true),
+    e.preventDefault(),
+    fd = new FormData(_id("form")),
+    send({ appid: fd.get("appid"), msg: fd.get("msg") })
+);
+
+const init = () => {
+    const appid = _q("appid")
+    if (appid !== null) {
+        _id("appid").value = appid;
+    }
+}
+
+// Actions
+_id("action-send").addEventListener("click", main);
+
+// Go!
+init();
+_id("readiness").classList.add("success");
