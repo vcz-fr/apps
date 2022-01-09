c13a804737ff6a14d95164a5a5bbac04362114d1
Vincenzo Scalzi <vcz-fr@users.noreply.github.com>
2022-01-09 21:31:58 +0100

    New website structure
    Also include function code!

---
+++ functions/api/feedback.ts
@@ -0,0 +1,57 @@
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Max-Age': '7200'
+};
+
+let CHANNELS: any;
+const ROOMS = {
+    "wB5cWzWsLvjD": { channel: "meetups", username: "BBL: FaaS apps" },
+    "8mVUhdPeI9T4": { channel: "meetups", username: "Meet: Cloud & AWS" },
+    "b0GlOIPeLX00": { channel: "apps", username: "Fowler" },
+    "crl4HX7hHtGc": { channel: "apps", username: "Feedback" },
+    "jM85ke3GcMUb": { channel: "apps", username: "Unlink" },
+    "DW7RbJ8FWHm5": { channel: "content", username: "Blog" },
+    "hS7YejNaDu6k": { channel: "content", username: "TheFollowUp" }
+};
+
+export const onRequestOptions = async () => new Response(null, { status: 204, headers: CORS_HEADERS });
+
+// Send a message to a discord channel through a webhook
+const sendDiscordMsg = async (appid: string, content: any) => {
+    if (!ROOMS.hasOwnProperty(appid)) {
+        throw "This topic does not exist";
+    }
+
+    const room = ROOMS[appid as keyof typeof ROOMS];
+
+    await fetch(`https://discordapp.com/api/webhooks/${CHANNELS[room.channel]}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            content,
+            username: room.username
+        })
+    });
+}
+
+// Retrieve the message contents and FWD to Discord!
+export const onRequestPost: PagesFunction = async (ctx) => {
+    if (ctx.request.headers.get("content-type") !== "application/json") {
+        return new Response(null, { status: 400, headers: CORS_HEADERS });
+    }
+
+    const { msg, appid } = await ctx.request.json();
+    CHANNELS = JSON.parse((ctx.env as any).HOOKS);
+
+    if (!!msg && !!appid) {
+        try {
+            await sendDiscordMsg(appid, msg);
+        } catch (e) {
+            return new Response(null, { status: 400, headers: CORS_HEADERS });
+        }
+        return new Response(null, { status: 204, headers: CORS_HEADERS });
+    }
+}
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+layout: default
+active: index
+---
+
+<div class="card">
+{%- capture text -%}
+
+# Apps: simple yet powerful!
+
+Hello, Vincenzo **Scalzi** here!
+
+Have you ever needed an app that just does one simple thing? Me too! Well, here is my contribution with hopefully
+exclusive apps and an oath: no added telemetry from my side!
+
+My challenge: offer stateless apps hosted on minimalistic environments. No server to manage, infinitely scalable and
+extreme performance! How does it work? Like my [personal website](https://vcz.fr), my [blog](https://blog.vcz.fr) and my [Meetup notes](https://meetups.vcz.fr),
+this site is hosted and delivered by [Cloudflare](https://www.cloudflare.com/){:rel="nofollow"}, and uses [Jekyll](https://jekyllrb.com/){:rel="nofollow"}
+to generate content! The difference is that it relies on [Cloudflare Workers](https://workers.cloudflare.com/){:rel="nofollow"}.
+
+If you like what you saw, do not forget to [leave some feedback](https://apps.vcz.fr/feedback/?appid=hS7YejNaDu6k) so
+that I can make the apps better.
+
+Have a nice visit!
+
+{%- endcapture -%}
+{{ text | markdownify }}
+</div>
